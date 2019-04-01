cfb403190ab7265691e52778a701fb564e6ac852
Georgia Lumley <georgia@Georgias-MacBook-Pro.local>
2019-04-01 15:07:39 +0100

    topic list shows topics and descriptions

--- front-end-knews/src/components/topicList/Topics.jsx
+++ front-end-knews/src/components/topicList/Topics.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 
 const Topics = ({ topics }) => {
-  console.log(topics);
-  return <p>he</p>;
+  return (
+    <ul>
+      {topics.map(topic => {
+        return (
+          <li key={topic.slug}>
+            <h3>{topic.slug}</h3>
+            <h4>{topic.description}</h4>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default Topics;
