8458bca991d6b10eca5eceea42322692dbc8b6e3
Georgia Lumley <georgia@Georgias-MacBook-Pro.local>
2019-04-02 12:57:51 +0100

    path for article:article_id added

--- front-end-knews/src/App.js
+++ front-end-knews/src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
-
+import { Router } from "@reach/router";
 import "./App.css";
+
 import Title from "./components/title";
-import TopicList from "./components/topicList/TopicList";
+
 import Articles from "./components/articles/Articles";
+import Article from "./components/article/Article";
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <Title />
-        <TopicList />
-        <Articles />
+
+        <Router className='app-main-route'>
+          <Articles path='/' />
+          <Article path='/articles/:article_id' />
+        </Router>
       </div>
     );
   }
