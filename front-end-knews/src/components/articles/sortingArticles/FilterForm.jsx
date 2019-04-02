b8fe5ac93ece538c0c2c92638b39c85af7a958e1
Georgia Lumley <georgia@Georgias-MacBook-Pro.local>
2019-04-02 13:00:56 +0100

    asc and desc swaped as desc is default

--- front-end-knews/src/components/articles/sortingArticles/FilterForm.jsx
+++ front-end-knews/src/components/articles/sortingArticles/FilterForm.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 
-const FilterForm = ({ updateState }) => {
+const FilterForm = ({ updateState, filterArticles }) => {
   return (
     <form>
       <select onChange={updateState} name='order'>
-        <option value='ascending'>ascending</option>
-        <option value='descending'>descending</option>
+        <option value='desc'>descending</option>
+        <option value='asc'>ascending</option>
       </select>
-      <select name='sortBy'>
+      <select onChange={updateState} name='sortBy'>
         <option value='title'>Title</option>
         <option value='votes'>Votes</option>
         <option value='created_at'>Date</option>
       </select>
+      <button onClick={filterArticles} type='submit'>
+        filter
+      </button>
     </form>
   );
 };
