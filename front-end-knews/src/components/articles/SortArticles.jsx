4b4948d857b4bc59c478500672c4dc86b62e319e
Georgia Lumley <georgia@Georgias-MacBook-Pro.local>
2019-04-02 13:00:39 +0100

    filters for articles added

--- front-end-knews/src/components/articles/SortArticles.jsx
+++ front-end-knews/src/components/articles/SortArticles.jsx
@@ -3,41 +3,39 @@ import React, { Component } from "react";
 
 class SortArticles extends Component {
   state = {
-    filter: { order: "ascending" }
+    filter: { order: "ascending", sortBy: "created_at" }
   };
   render() {
     return (
       <div>
-        <FilterForm updateState={this.changeStateByFilterOptions} />
+        <FilterForm
+          updateState={this.changeStateByFilterOptions}
+          filterArticles={this.filterArticles}
+        />
       </div>
     );
   }
-  changeStateByFilterOptions(event) {
+  changeStateByFilterOptions = event => {
     const filterOption = event.target.value;
-    if (filterOption === "ascending" || filterOption === "descending") {
-      this.setState({ filter: { order: filterOption } });
-    } else {
-      
-    }
-  }
-  componentDidUpdate() {
 
-  }
+    if (filterOption === "ascending" || filterOption === "descending") {
+      this.setState(prevState => {
+        return { filter: { ...prevState.filter, order: filterOption } };
+      });
+    }
+    if (
+      filterOption === "title" ||
+      filterOption === "votes" ||
+      filterOption === "created_at"
+    ) {
+      this.setState(prevState => {
+        return { filter: { ...prevState.filter, sortBy: filterOption } };
+      });
+    }
+  };
+  filterArticles = event => {
+    event.preventDefault();
+  };
 }
 
 export default SortArticles;
-
-// const sortBy = event => {
-//   const sortBy = event.target.value;
-
-//   if (sortBy === "ascending") {
-//     axios
-//       .get("https://nc-knews-lumley.herokuapp.com/api/articles?order=asc")
-//       .then(({ data }) => updateState(data));
-//   } else if (sortBy === "descending") {
-//   } else {
-//   }
-// };
-// }
-
-// export default SortArticles;
