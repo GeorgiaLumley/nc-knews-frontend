0118f411d1a6f52e32539ec6bb8640ad0f9d064f
Georgia Lumley <georgia@Georgias-MacBook-Pro.local>
2019-04-02 12:59:51 +0100

    artciles  back together. state needed to be shared

--- front-end-knews/src/components/articles/Articles.jsx
+++ front-end-knews/src/components/articles/Articles.jsx
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
 import axios from "axios";
 import DisplayArticles from "../articles/DisplayArticles";
-import SortArticles from "./SortArticles";
+import TopicList from "../topicList/TopicList";
+import FilterForm from "./sortingArticles/FilterForm";
 
 class Articles extends Component {
   state = {
-    articles: []
+    articles: [],
+    filter: { order: "desc", sortBy: "created_at" }
   };
   render() {
     return (
       <div>
+        <TopicList />
         <DisplayArticles articles={this.state.articles} />
-        <SortArticles />
+        <FilterForm
+          updateState={this.changeStateByFilterOptions}
+          filterArticles={this.filterArticles}
+        />
       </div>
     );
   }
@@ -20,9 +26,37 @@ class Articles extends Component {
       .get("https://nc-knews-lumley.herokuapp.com/api/articles")
       .then(res => this.setState({ articles: res.data.articles }));
   }
-  updateState(data) {
-    return <h1>update!!</h1>;
-  }
+
+  changeStateByFilterOptions = event => {
+    const filterOption = event.target.value;
+
+    if (filterOption === "asc" || filterOption === "desc") {
+      this.setState(prevState => {
+        return { filter: { ...prevState.filter, order: filterOption } };
+      });
+    }
+    if (
+      filterOption === "title" ||
+      filterOption === "votes" ||
+      filterOption === "created_at"
+    ) {
+      this.setState(prevState => {
+        return { filter: { ...prevState.filter, sortBy: filterOption } };
+      });
+    }
+  };
+  filterArticles = event => {
+    event.preventDefault();
+    const order = this.state.filter.order;
+    const sortBy = this.state.filter.sortBy;
+    axios
+      .get(
+        `https://nc-knews-lumley.herokuapp.com/api/articles?order=${order}&&sortBy=${sortBy}`
+      )
+      .then(({ data }) => {
+        this.setState({ articles: data.articles });
+      });
+  };
 }
 
 export default Articles;
