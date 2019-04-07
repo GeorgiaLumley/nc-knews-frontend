2664e0a718cadbd98dc541bbab54b7069d07e472
Georgia Lumley <georgia@georgias-mbp.home>
2019-04-07 09:21:57 +0100

    filterForm styling added

--- front-end-knews/src/components/articles/sortingArticles/FilterForm.jsx
+++ front-end-knews/src/components/articles/sortingArticles/FilterForm.jsx
@@ -2,17 +2,27 @@ import React from "react";
 
 const FilterForm = ({ updateState, filterArticles }) => {
   return (
-    <form>
-      <select className='selectBox' onChange={updateState} name='order'>
+    <form id='filterForm'>
+      <select
+        className='selectBox'
+        id='order'
+        onChange={updateState}
+        name='order'
+      >
         <option value='desc'>descending</option>
         <option value='asc'>ascending</option>
       </select>
-      <select onChange={updateState} name='sortBy'>
+      <select
+        className='selectBox'
+        id='filterBy'
+        onChange={updateState}
+        name='sortBy'
+      >
         <option value='title'>Title</option>
         <option value='votes'>Votes</option>
         <option value='created_at'>Date</option>
       </select>
-      <button className='button' onClick={filterArticles} type='submit'>
+      <button className='button' id="filterButton"onClick={filterArticles} type='submit'>
         filter
       </button>
     </form>
