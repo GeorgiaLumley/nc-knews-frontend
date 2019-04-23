26a1bce8b6c0745790475cf554ba4a58dbf38c09
Georgia Lumley <georgia@Georgias-MacBook-Pro.local>
2019-04-23 12:47:36 +0100

    remove link import

--- front-end-knews/src/App.js
+++ front-end-knews/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import { fetchAllUsers } from "./api";
 import "./App.css";
 import Title from "./components/title";
 import Articles from "./components/articles/Articles";
 import Article from "./components/article/Article";
 import TopicList from "./components/topicList/TopicList";
-
 import CreateArticle from "./components/CreateArticle/CreateArticle";
 import UserProfile from "./components/profile/UserProfile";
 import PageNotFound from "./components/404";
@@ -26,11 +25,11 @@ class App extends Component {
           data-cy='title'
           loggedIn={this.state.loggedIn}
           username={this.state.username}
-          openTopics={this.openTopics}
+          openTopics={this.openTopicsState}
           topicState={this.state.openTopics}
         />
-        <p id='guest'>Guest Username: tickle122</p>
-        {!this.state.loggedIn ? (
+        {!this.state.loggedIn && <p id='guest'>Guest Username: tickle122</p>}
+        {!this.state.loggedIn && !this.state.openTopics ? (
           <div id='loginInput'>
             <label>
               Username
@@ -50,18 +49,20 @@ class App extends Component {
             </label>
           </div>
         ) : (
-          <span id='loggedIn'>
-            <p id='loggedInUser'>{`logged in as ${this.state.username}`}</p>
+          !this.state.openTopics && (
+            <span id='loggedIn'>
+              <p id='loggedInUser'>{`logged in as ${this.state.username}`}</p>
 
-            <button
-              onClick={this.LoggedOut}
-              className='button'
-              id='logOutButton'
-            >
-              Log Out
-            </button>
-            {this.state.loggedIn && <CreateArticleButton />}
-          </span>
+              <button
+                onClick={this.LoggedOut}
+                className='button'
+                id='logOutButton'
+              >
+                Log Out
+              </button>
+              {this.state.loggedIn && <CreateArticleButton />}
+            </span>
+          )
         )}
         {this.state.validUsername && !this.state.loggedIn ? (
           <p className='errMsg'>Invalid UserName</p>
@@ -69,7 +70,7 @@ class App extends Component {
           <> </>
         )}
         <Router className='app-main-route'>
-          <TopicList path='/topics' />
+          <TopicList path='/topics' openTopics={this.openTopicsState} />
           <Articles
             path='/'
             user={this.state.username}
@@ -96,6 +97,11 @@ class App extends Component {
       </div>
     );
   }
+  componentDidMount() {
+    console.log("state");
+    this.setState({ openTopics: false });
+  }
+
   usernameChange = e => {
     const username = e.target.value;
     this.setState({ username });
@@ -117,13 +123,13 @@ class App extends Component {
   LoggedOut = () => {
     this.setState({ loggedIn: false });
   };
-  openTopics = () => {
-    console.log("app");
-    if (this.state.openTopics === true) {
-      this.setState(prevState => {
-        return { openTopics: true };
-      });
-    }
+  openTopicsState = () => {
+    console.log("hi");
+    this.setState(prevState => {
+      console.log(prevState.openTopics);
+
+      return { openTopics: !prevState.openTopics };
+    });
   };
 }
 
