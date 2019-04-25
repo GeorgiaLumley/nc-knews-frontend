9f9d71c10ba3d356936bfe87b5ae9ab1bcd879ab
Georgia Lumley <georgia@Georgias-MacBook-Pro.local>
2019-04-25 09:52:06 +0100

    shows error if there is no articles

--- front-end-knews/src/components/articles/DisplayArticles.jsx
+++ front-end-knews/src/components/articles/DisplayArticles.jsx
@@ -3,19 +3,25 @@ import { Link } from "@reach/router";
 
 const DisplayArticles = ({ articles }) => {
   return (
-    <ul className='articleList' data-cy='articlesList'>
-      {articles.map(article => {
-        return (
-          <li className='articleItem' key={article.article_id}>
-            <Link to={`/articles/${article.article_id}`}>
-              <h3 id='articleTitle'>{article.title}</h3>
-            </Link>
-            <p id='articleAuthor'>Author: {article.author}</p>
-            <p id='articleTopic'>Topic: {article.topic}</p>
-          </li>
-        );
-      })}
-    </ul>
+    <div>
+      {articles.length === 0 ? (
+        <p>You have no articles</p>
+      ) : (
+        <ul className='articleList' data-cy='articlesList'>
+          {articles.map(article => {
+            return (
+              <li className='articleItem' key={article.article_id}>
+                <Link to={`/articles/${article.article_id}`}>
+                  <h3 id='articleTitle'>{article.title}</h3>
+                </Link>
+                <p id='articleAuthor'>Author: {article.author}</p>
+                <p id='articleTopic'>Topic: {article.topic}</p>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
   );
 };
 
