7301701d78f469433eefeadb8bd5d836e6c3276b
Georgia Lumley <georgia@Georgias-MacBook-Pro.local>
2019-04-26 11:58:50 +0100

    add id to users comments div

--- front-end-knews/src/components/profile/UserComments.jsx
+++ front-end-knews/src/components/profile/UserComments.jsx
@@ -12,7 +12,7 @@ const UserComments = ({ comments, user }) => {
             return (
               <div key='userCommentsDiv'>
                 <li key={comment.comment_id} id='userCommentItem'>
-                  <div>
+                  <div id='UserCommentsDiv'>
                     <Link to={`/articles/${comment.article_id}`}>
                       <p id='commentAuthor'>{comment.author}</p>
                       <p id='commentBody'>{comment.body}</p>
